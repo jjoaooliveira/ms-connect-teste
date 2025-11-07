ed0c0cbd90dae94a14dfec4191e2e88f093c8376
Joao Oliveira <joaooliv11@hotmail.com>
2025-11-07 09:54:52 -0400

    feat: create api

---
+++ app-crud/backend/api/app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone'
+    ];
+}
